ebf6abd50ee750b191542235b2eab8e519058cfa
#1 wabungus fan <zacharym234@deltalearns.ca>
2022-01-25 14:32:34 -0800

    yuh

--- yup.py
+++ yup.py
@@ -1,2 +1,14 @@
-while True:
-    print("ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ")
+import tkinter as tk 
+from tkinter import *
+from tkinter import ttk
+
+
+
+
+window = tk.Tk()
+window.title("dubious little creature")
+sus = PhotoImage(file="sus.jpg")
+label1 = tk.Label(window, image=sus)
+label1.grid(column=0, row=0) 
+
+window.mainloop()
